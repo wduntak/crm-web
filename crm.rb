1b812623160f0b543cc78e1bf0f30d7c2583efb4
wduntak <wduntak@gmail.com>
2014-10-28 13:30:39 -0400

    Created a instance of rolodex and replaced the contacts array.

--- crm.rb
+++ crm.rb
@@ -2,16 +2,14 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+$rolodex = Rolodex.new
+
+#routes
 get '/' do
 	@crm_app_name = "My CRM"
 	erb :index
 end
 
 get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
-
   erb :contacts
 end
