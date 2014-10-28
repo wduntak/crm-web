1a83e759ea88c17c43c87c0337db9605e401827f
wduntak <wduntak@gmail.com>
2014-10-28 13:25:38 -0400

    Added html to the contacts.erb

--- crm.rb
+++ crm.rb
@@ -6,6 +6,11 @@ get '/' do
 	erb :index
 end
 
-get '/contacts' do
+get "/contacts" do
+  @contacts = []
+  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
 
+  erb :contacts
 end
