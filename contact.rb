87bcaf44135f6f6082bee66078147b5f1507f7b7
wduntak <wduntak@gmail.com>
2014-10-28 14:27:25 -0400

    Fixed error with form entry, declared :id

--- contact.rb
+++ contact.rb
@@ -1,10 +1,10 @@
 class Contact
-	attr_accessor :first_name, :last_name, :email, :note
+	attr_accessor :first_name, :last_name, :email, :note, :id
 
 	def initialize(first_name, last_name, email, note)
 		@first_name = first_name
 		@last_name = last_name
 		@email = email
-		@note = email
+		@note = note
 	end
 end
