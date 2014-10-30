fdd87b7cc64d8d085ae4862ac612f10b7c28da3a
wduntak <wduntak@gmail.com>
2014-10-30 13:27:20 -0400

    Added a edit function to the crm

--- crm.rb
+++ crm.rb
@@ -34,4 +34,26 @@ get '/contacts/:id' do
   end
 end
 
+put '/contacts/:id' do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/contacts/:id/edit' do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact, :layout => :layout
+  else
+    raise Sinatra::NotFound
+  end
+end
 
